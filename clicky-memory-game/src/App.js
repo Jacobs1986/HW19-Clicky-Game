0948beb9f919c7a16a7d85081269c452dee40a11
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-20 11:07:27 -0600

    Update App.js
    Created a map that will now render the images in the json file.

--- clicky-memory-game/src/App.js
+++ clicky-memory-game/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Nav from "./components/Navigation"
+import CharacterCard from "./components/characters"
+import characters from "./characters.json";
 
 class App extends React.Component {
+    state = {
+        characters: characters
+    }
+
     render() {
         return (
-            // Information goes here
+            <>
             <Nav />
+            {this.state.characters.map(characters =>
+            <CharacterCard 
+                key = {characters.id}
+                name = {characters.name}
+                image = {characters.image}
+            />)}
+            </>
         )
     }
 }
